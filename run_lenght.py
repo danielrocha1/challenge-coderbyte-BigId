dc7ed12101272e68835e49ae5929be6ee07753c8
Daniel Rocha <daniel.rochats@gmail.com>
2021-05-16 18:27:54 -0300

    Add files via upload

---
+++ run_lenght.py
@@ -0,0 +1,17 @@
+arg = "aaabbccccddd"
+def runlenght(arg):
+    result = ''
+    last_char = ''
+    count = 1
+
+    for char in arg:
+        if char != last_char:
+            if last_char:
+                result += str(count) + last_char
+            count = 1
+            last_char = char
+        else:
+            count += 1
+    result += str(count) +  last_char
+    return result
+print(runlenght(arg))
